008b94c2f28aebd7203e943a2ce88f61b8c3b5ca
Celina CHEBALLAH <160405742+Ktitoo@users.noreply.github.com>
2024-03-17 22:52:23 +0100

    Update Instruction.java
    Dernière version Instruction

--- Instruction.java
+++ Instruction.java
@@ -1,4 +1,9 @@
+/**
+ * L'énumération Instruction définit différentes instructions avec leur nombre d'arguments requis.
+ * Chaque instruction est associée à un nombre spécifique d'arguments requis et à un nom.
+ */
 public enum Instruction {
+    // Différentes instructions avec leur nombre d'arguments requis
     COPY(2, "COPY"),
     ADDI(3, "ADDI"),
     MULI(3, "MULI"),
@@ -13,9 +18,10 @@ public enum Instruction {
     LINK(1, "LINK"),
     HALT(0, "HALT");
 
-    private int requiredArgs;
-    private String name;
+    private int requiredArgs;  // Nombre d'arguments requis pour cette instruction
+    private String name;      // Nom de l'instruction
 
+    // Constructeur de l'énumération Instruction
     Instruction(int requiredArgs, String name) {
         this.requiredArgs = requiredArgs;
         this.name = name;
