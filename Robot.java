f16242b220bef6a190258360247bf4aea41e8fa5
Celina CHEBALLAH <160405742+Ktitoo@users.noreply.github.com>
2024-03-17 22:53:37 +0100

    Update Robot.java
    Dernière version Robot

--- Robot.java
+++ Robot.java
@@ -1,9 +1,13 @@
+/**
+ * La classe Robot représente un robot dans un environnement de grille. Elle utilise la classe Grid pour accéder à la grille
+ * et effectuer des opérations liées à la position du robot.
+ */
 public class Robot {
     private final String name;
-    private final Registre X, T;
-    private int posX, posY, pos;
+    private final Registre X, T;   // Registres standards du robot.
+    private int posX, posY, pos;   // la position en X (horizontale) , la position en Y (verticale), pos : position du robot dans la grille
     private final int initX, initY;
-    private int pointer;
+    private int pointer;          // position actuelle d'exécution dans le programme
     private Grid grid;
 
     public Robot(String name, Registre X, Registre T, int posX, int posY, Grid grid) {
@@ -12,51 +16,53 @@ public class Robot {
         this.T = T;
         this.posX = posX;
         this.posY = posY;
+        // Convertir la position horizontale (posX) en une valeur numérique en multipliant par la taille de la grille (supposée être 5),
+        // puis ajouter la position verticale (posY); ceci prend en compte à la fois posX et posY dans la grille.
         this.pos = posX * 5 + posY;
         this.pointer = 0;
         this.grid = grid;
-        initX = posX;
-        initY = posY;
+        this.initX = posX;
+        this.initY = posY;
     }
 
     public String getName() {
-        return name;
+        return this.name;
     }
 
     public Registre getX() {
-        return X;
+        return this.X;
     }
 
     public Registre getT() {
-        return T;
+        return this.T;
     }
 
     public int getPosX() {
-        return posX;
+        return this.posX;
     }
 
     public int getPosY() {
-        return posY;
+        return this.posY;
     }
 
     public int getPos() {
-        return pos;
+        return this.pos;
     }
 
     public Grid getGrid() {
-        return grid;
+        return this.grid;
     }
 
     public int getPointer() {
-        return pointer;
+        return this.pointer;
     }
 
     public int getInitX() {
-        return initX;
+        return this.initX;
     }
 
     public int getInitY() {
-        return initY;
+        return this.initY;
     }
 
     public void setPosX(int posX) {
@@ -74,21 +80,23 @@ public class Robot {
     public void setPointer(int pointer) {
         this.pointer = pointer;
     }
-
+    
+    // réinitialisation des valeurs du robot.
     public void reset() {
-        setPointer(0);
-        setPosX(initX);
-        setPosY(initY);
-        setPos(initX * 5 + initY);
-        getT().reset();
-        getX().reset();
+        this.setPointer(0);
+        this.setPosX(this.initX);
+        this.setPosY(this.initY);
+        this.setPos(this.initX * 5 + this.initY);
+        this.getT().reset();
+        this.getX().reset();
     }
 
+    // afficher l'état actuel du robot.
     @Override
     public String toString() {
         return "Robot{" +
-                "X:" + X.getValue() +
-                ", T:" + T.getValue() +
+                "X:" + this.X.getValue() +
+                ", T:" + this.T.getValue() +
                 '}';
     }
 }
